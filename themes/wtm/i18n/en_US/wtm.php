73ec7d86e9128bc4168968060074d3356ee199b5
Dylan Gillespie <dylan@ethertontech.com>
2013-05-23 16:40:08 -0600

    started work on moving divs

--- themes/wtm/i18n/en_US/wtm.php
+++ themes/wtm/i18n/en_US/wtm.php
@@ -3,4 +3,6 @@ $lang = array(
 		'Layer Meta-Data'=>'Layer Meta-Data',
 		'parent layer'=> 'Parent Layer',
 		'none'=>'None',
+		'investigations' => 'Investigations',
+		'submit' => 'How to Report',
 );
