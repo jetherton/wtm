804a580a398de724b10d4e69a17f60d91fbbbd21
Dylan Gillespie <dylan@ethertontech.com>
2013-05-27 10:07:03 -0600

    created search plugin

---
+++ plugins/searchlocation/views/searchlocation/searchlocation_js.php
@@ -0,0 +1,234 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/***********************************************************
+* mapmeasure_js.php - Javascript for Map Measure Plugin
+* This software is copy righted by WatchTheMed 2013
+* Writen by Dylan Gillespie, Etherton Technologies <http://ethertontech.com>
+* Started on 2013-30-04
+* This plugin is to add a ruler tool to the maps.
+*************************************************************/
+?>
+
+<script type="text/javascript">	
+	var path_info = '<?php 
+		if ((strpos(url::current(), 'reports/view')) !== false){ echo 'reports/view';}
+		elseif ((strpos(url::current(), 'admin/reports/edit')) !== false){ echo 'admin/reports/edit';}
+		else {echo url::current(); }	?>';
+	var map_div = '';
+	var my_map = null;
+	var map_search = false;
+	var ruler_exists = false;
+	//variables to hold map zooming listeners
+	var clickOut;
+	var clickIn;
+
+	$(document).ready(function(){
+		$('a.map').click(function(){
+			if(!map_search){
+				if(!ruler_exists){
+					Ruler();
+					zoomButtons();
+				}
+				else{
+					$('#rulerControl').show();
+				}
+				map_search = true;
+			}
+		});
+		$('a.list').click(function(){
+			if(map_search){
+				map_search = false;
+				$('#rulerControl').hide();
+			}
+		});
+	});
+	
+
+
+    //turn off all listeners
+    function deactivateAll(){
+		clickOut.deactivate();
+		clickIn.deactivate();
+		$('#'+map_div).css({
+			'cursor': "default"
+		});
+		$('#output').hide();
+		for(key in measureControls) {
+            var control = measureControls[key];
+            control.deactivate();
+            my_map.removeControl(control);
+            //control.destroy();
+        }
+    }
+
+	function createRuler(){
+		//create the ruler buttons
+		$('#'+map_div).before(
+				'<div style="position:absolute;">\
+				<div id="toolbarControl">\
+						<div title="Measure in a series of lines." id="lineDraw" onclick="toggleControl(this)">\
+							<img class="rulerIcon" src="<?php echo URL::base();?>plugins/mapmeasure/media/img/img_trans.gif" width="1" height="1"/>\
+						</div>\
+						<div title="Measure within an area." id="areaDraw" onclick="toggleControl(this)">\
+							<img class="areaIcon" src="<?php echo URL::base();?>plugins/mapmeasure/media/img/img_trans.gif" width="1" height="1"/>\
+						</div>\
+						<div value="None" title="Turn off measuring." id="noDraw">\
+							<img class="dragIcon" src="<?php echo URL::base();?>plugins/mapmeasure/media/img/img_trans.gif" width="1" height="1"/>\
+						</div>\
+						</br><div id = "output"></div>\
+				</div>\
+				');
+		$('#lineDraw').val('line');
+		$('#areaDraw').val('polygon');
+		$('#noDraw').click(function(){
+			$('#output').hide();
+			deactivateAll();
+		});		
+
+		$('#filter-menu-toggle').css({"left": "185px"});
+		$('#layers-menu-toggle').css({"left": "346px"});
+	}
+
+    
+    function handleMeasurements(event) {
+        var units = event.units;
+        var order = event.order;
+        var measure = event.measure;
+        var element = document.getElementById('output');
+        var out = "";
+        if(order == 1) {
+            out += "Distance: " + measure.toFixed(3) + " " + units;
+        } else {
+            out += "Distance: " + measure.toFixed(3) + " " + units + "<sup>2</sup>";
+        }
+        element.innerHTML = out;
+        $('#output').show();
+        
+    }
+
+    function toggleControl(element) {
+        clickIn.deactivate();
+        clickOut.deactivate();
+        
+        $('#'+map_div).css({
+			'cursor': "default"
+		});
+        for(key in measureControls) {
+            var control = measureControls[key];
+            if(element.value == key) {
+                control.activate();
+            } else {
+                control.deactivate();
+            }
+        }
+        //console.log(my_map);
+    }
+ 
+
+
+  //create the ZoomButtons
+	function zoomButtons(){
+		$('.olControlZoom').hide();
+
+		$('#lineDraw').before(
+			'<div id="clickIn" title="Zoom in"><img class="zoomInIcon" src="<?php echo URL::base();?>plugins/mapmeasure/media/img/img_trans.gif" width="1" height="1"/></div>\
+			<div id="clickOut" title="Zoom out"><img class="zoomOutIcon" src="<?php echo URL::base();?>plugins/mapmeasure/media/img/img_trans.gif" width="1" height="1"/></div>\
+			'
+  		);
+
+        OpenLayers.Control.ClickOut = OpenLayers.Class(OpenLayers.Control, {                
+            defaultHandlerOptions: {
+                'single': true,
+                'double': false,
+                'pixelTolerance': 0,
+                'stopSingle': false,
+                'stopDouble': true
+            },
+            initialize: function(options) {
+                this.handlerOptions = OpenLayers.Util.extend(
+                    {}, this.defaultHandlerOptions
+                );
+                OpenLayers.Control.prototype.initialize.apply(
+                    this, arguments
+                ); 
+                this.handler = new OpenLayers.Handler.Click(
+                    this, {
+                        'click': this.trigger
+                    }, this.handlerOptions
+                );
+            }, 
+            trigger: function(e) {
+            	var lonlat = my_map.getLonLatFromPixel(e.xy);
+            	my_map.setCenter(lonlat, my_map.getZoom() - 1);
+            }
+        });
+        
+        OpenLayers.Control.ClickIn = OpenLayers.Class(OpenLayers.Control, {                
+            defaultHandlerOptions: {
+                'single': true,
+                'double': false,
+                'pixelTolerance': 0,
+                'stopSingle': false,
+                'stopDouble': true
+            },
+
+            initialize: function(options) {
+                this.handlerOptions = OpenLayers.Util.extend(
+                    {}, this.defaultHandlerOptions
+                );
+                OpenLayers.Control.prototype.initialize.apply(
+                    this, arguments
+                ); 
+                this.handler = new OpenLayers.Handler.Click(
+                    this, {
+                        'click': this.trigger
+                    }, this.handlerOptions
+                );
+            }, 
+
+            trigger: function(e) {
+            	var lonlat = my_map.getLonLatFromPixel(e.xy);
+            	my_map.setCenter(lonlat, my_map.getZoom() + 1);
+            }
+
+        });
+
+        clickOut = new OpenLayers.Control.ClickOut();
+        clickIn = new OpenLayers.Control.ClickIn();
+
+        my_map.addControl(clickOut);
+        my_map.addControl(clickIn);
+        $('#clickOut').click(function(){
+        	deactivateAll()
+			clickOut.activate();
+			$('#'+map_div).css({
+				'cursor': "url('<?php echo URL::base()?>plugins/mapmeasure/media/img/ZoomOut.png'), -moz-zoom-out"
+			});
+			//console.log(my_map);
+		});	
+		$('#clickIn').click(function(){
+			deactivateAll();
+			clickIn.activate();
+			$('#'+map_div).css({
+				'cursor': "url('<?php echo URL::base()?>plugins/mapmeasure/media/img/ZoomIn.png'), -moz-zoom-in"
+			});
+			
+		});	
+  	}
+    
+
+jQuery(window).load(function() {
+	if(path_info != 'reports'){
+		Ruler();
+		zoomButtons();
+		
+	}
+});
+	
+	
+</script>
+
+
+
+<link rel="stylesheet" type="text/css" href="<?php echo url::base(); ?>plugins/mapmeasure/media/css/measureCSS.css"/>
+
+
