7d0f0f027e403daa3f6964cc98be92217e2942b6
John Etherton <john@ethertontech.com>
2013-05-30 23:11:45 +0000

    now this works on the /admin/reports/edit page too.

--- plugins/reportslayers/hooks/reportslayers.php
+++ plugins/reportslayers/hooks/reportslayers.php
@@ -19,16 +19,23 @@ class reportslayers {
 	{
 		if(Router::$controller == "reports" AND Router::$method == "view")
 		{
-        	Event::add('ushahidi_action.report_view_sidebar', array($this, '_add_view_ui'));
-        	Event::add('ushahidi_action.header_scripts', array($this, '_add_view_js'));                
-                plugin::add_stylesheet("reportslayers/media/css/reportslayers");
+                    Event::add('ushahidi_action.report_view_sidebar', array($this, '_add_view_ui'));
+                    Event::add('ushahidi_action.header_scripts', array($this, '_add_view_js'));                
+                    plugin::add_stylesheet("reportslayers/media/css/reportslayers");
                 }
                 
                 if(Router::$controller == "reports" AND Router::$method == "submit")
 		{
-        	Event::add('ushahidi_action.report_form_frontend_after_time', array($this, '_add_view_ui'));
-        	Event::add('ushahidi_action.header_scripts', array($this, '_add_view_js'));                
-                plugin::add_stylesheet("reportslayers/media/css/reportslayers_submit");
+                    Event::add('ushahidi_action.report_form_frontend_after_time', array($this, '_add_view_ui'));
+                    Event::add('ushahidi_action.header_scripts', array($this, '_add_view_js'));                
+                    plugin::add_stylesheet("reportslayers/media/css/reportslayers_submit");
+                }
+                
+                   if(Router::$controller == "reports" AND Router::$method == "edit")
+		{
+                    Event::add('ushahidi_action.report_pre_form_admin', array($this, '_add_view_ui'));
+                    Event::add('ushahidi_action.header_scripts_admin', array($this, '_add_view_js'));                
+                    plugin::add_stylesheet("reportslayers/media/css/reportslayers_edit");
                 }
 
 
