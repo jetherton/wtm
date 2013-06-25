172e27ec3653817021604fce44c4d004696bd6e8
John Etherton <john@ethertontech.com>
2013-06-25 21:30:19 +0000

    Fixed a silly mistake where I didn't clear the database if the POST param was left blank. But I certainly want to clear the DB when the post is blank, that means the user doesn't want to select any categories.'

--- plugins/defaultcategories/views/defaultcategories/defaultsubmit.php
+++ plugins/defaultcategories/views/defaultcategories/defaultsubmit.php
@@ -7,22 +7,22 @@
 * This plugin is to allow default categories for maps.
 *************************************************************/
 
+    //first turn off category_default for all categories that have it on
+    $allCats = ORM::factory('category')->where('category_default',1)->find_all();
+    foreach($allCats as $cat){
+	$cat->category_default = 0;
+	$cat->save();
+    }
 
 
-if(isset($post['defaultCats'])){
-    
-	//first turn off category_default for all categories that have it on
-	$allCats = ORM::factory('category')->where('category_default',1)->find_all();
-	foreach($allCats as $cat){
-	    $cat->category_default = 0;
-	    $cat->save();
-	}
-	
+    if(isset($post['defaultCats'])){
+
+
 	//now turn it back on for those that are in the POST
 	foreach($post['defaultCats'] as $dbId=>$cat_item){
 	    $cat = ORM::factory('category')->where('id',$dbId)->find();
 	    $cat->category_default = 1;
 	    $cat->save();
 	}
-    
-}
+
+    } 
