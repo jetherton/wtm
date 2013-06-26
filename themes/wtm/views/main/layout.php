cbcda41eba8f01833eb1eb9055281092a260d116
John Etherton <john@ethertontech.com>
2013-06-26 00:33:40 +0000

    Moved the map status items to the correct place.

--- themes/wtm/views/main/layout.php
+++ themes/wtm/views/main/layout.php
@@ -102,13 +102,14 @@ $(function(){
 				?>
 				<!-- report type filters -->
 				<div id="report-type-filter" class="filters">
-				<h3><?php echo Kohana::lang('ui_main.type'); ?></h3>
+
 										<ul>
-											<li><a id="media_0" class="active" href="#"><span><?php echo Kohana::lang('ui_main.reports'); ?></span></a></li>
-											<li><a id="media_4" href="#"><span><?php echo Kohana::lang('ui_main.news'); ?></span></a></li>
-											<li><a id="media_1" href="#"><span><?php echo Kohana::lang('ui_main.pictures'); ?></span></a></li>
-											<li><a id="media_2" href="#"><span><?php echo Kohana::lang('ui_main.video'); ?></span></a></li>
+											<li><div>filter</div></li>
 											<li><a id="media_0" href="#"><span><?php echo Kohana::lang('ui_main.all'); ?></span></a></li>
+											<li><a id="media_4" href="#"></span></a></li>
+											<li><a id="media_1" href="#"></span></a></li>
+											<li><a id="media_2" href="#"></a></li>
+											
 										</ul>
 										<div class="floatbox">
 												<?php
